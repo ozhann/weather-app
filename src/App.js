9b4a62b56f1c53f437c71f5ce109f8fb6389e564
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-06-20 18:37:39 +0200

    render

--- src/App.js
+++ src/App.js
@@ -53,7 +53,46 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   };
 
-  return <div>hello</div>;
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
